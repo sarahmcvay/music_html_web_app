5226a7895e2a55b3e9262ee998b426c9edb20275
sarahmcvay <tosarah.s@gmail.com>
2026-01-11 22:51:42 +0000

    found and corrected typo in seed info

--- seeds/music_html_web_app.sql
+++ seeds/music_html_web_app.sql
@@ -32,7 +32,7 @@ INSERT INTO artists (name, genre) VALUES ('Nina Simone', 'Jazz');
 
 -- -- SEED INFORMATION ALBUMS
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Doolittle', 1989, 1);
-INSERT INTO albums (title, release_year, artist_id) VALUES ('Surfer Rosa', 1988, 1);
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Surfa Rosa', 1988, 1);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Waterloo', 1974, 2);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Super Trouper', 1980, 2);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Bossanova', 1990, 1);
@@ -43,3 +43,6 @@ INSERT INTO albums (title, release_year, artist_id) VALUES ('Baltimore', 1978, 4
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Here Comes the Sun', 1971, 4);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Fodder on My Wings', 1982, 4);
 INSERT INTO albums (title, release_year, artist_id) VALUES ('Ring Ring', 1973, 2);
+
+-- ### To seed the Database
+--     psql music_html_web_app < seeds/music_html_web_app.sql
